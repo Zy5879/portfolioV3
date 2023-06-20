2785be831f234693de2cd768a3d9316546312634
Zy5879 <ZaireM21@gmail.com>
2023-06-20 18:22:51 -0400

    addedLoader to canvas

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,11 +1,8 @@
-import { Sparkles, OrbitControls, Center } from "@react-three/drei";
+import { Sparkles, OrbitControls, Loader } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import AvatarModel from "../three/Avatar";
-import { HtmlText } from "../three/Html";
-import ThreeD from "../three/ThreeD";
-import { Html } from "@react-three/drei";
-import Navbar from "./Navbar";
 import { Perf } from "r3f-perf";
+import { Suspense } from "react";
 
 const Section = (props) => {
   const { children } = props;
@@ -22,13 +19,16 @@ const Section = (props) => {
 export default function WelcomeSection() {
   return (
     <>
+      <Loader />
       <Canvas>
         <Perf />
         <OrbitControls enableZoom={false} />
         <color args={["#000000"]} attach="background" />
         <directionalLight position={[1, 2, 3]} intensity={2.0} />
         <Sparkles count={2000} size={1.5} scale={20} />
-        <AvatarModel scale={2} position-y={-2} />
+        <Suspense fallback={null}>
+          <AvatarModel scale={2} position-y={-2} />
+        </Suspense>
         {/* <Html as="div" fullscreen>
           <Navbar />
         </Html> */}
